6e8538663a2f156526a577f3a9c5576e73d75cab
v0lt <mpc-be@mail.ru>
2024-12-29 20:35:20 +0300

    Синхронизация кода VirtualDub2.

--- vd2/system/h/stdafx.h
+++ vd2/system/h/stdafx.h
@@ -3,18 +3,7 @@
 // mmreg.h : warning C4819: The file contains a character that cannot be represented in the current code page (932). Save the file in Unicode format to prevent data loss
 #pragma setlocale("C")
 
-// Detect the Windows SDK in use and select Windows 2000 baseline
-// if the Vista SDK, else Windows 98 baseline.
-#ifdef _MSC_VER
-#include <ntverp.h>
-#else
-#define VER_PRODUCTBUILD 6001
-#endif
-#if VER_PRODUCTBUILD >= 6000
-#define _WIN32_WINNT 0x0501
-#else
-#define _WIN32_WINNT 0x0410
-#endif
+#define _WIN32_WINNT 0x0600
 
 #include <vd2/system/vdtypes.h>
 #include <vd2/system/atomic.h>
